1ec36a046cacf01b5c911ab3507b6b7c53cd39b3
Vasilen Tsvetkov <vasilen@7digit.io>
2022-11-10 00:16:07 +0000

    Create: PUT route to update client data

--- routes/v1/ClientRouter.js
+++ routes/v1/ClientRouter.js
@@ -9,25 +9,46 @@ const CLIENT_LOCAL_HOST = "http://localhost:3001";
 
 const CLIENT_URL = process.env.CLIENT_URL;
 
-router.route("/").get(authenticate, async (req, res) => {
-  /**
-   * #route   GET /api/v1/client
-   * #desc    Get current client data
-   */
+router
+  .route("/")
+  .get(authenticate, async (req, res) => {
+    /**
+     * #route   GET /api/v1/client
+     * #desc    Get current client data
+     */
 
-  const response = await fetch(`${CLIENT_URL}/client/v1/client${req.url}`, {
-    method: req.method,
-    headers: {
-      ...req.headers,
-      host: CLIENT_LOCAL_HOST,
-      "Content-type": "application/json",
-      "Cache-control": "no-cache",
-    },
-  }).catch(console.log);
+    const response = await fetch(`${CLIENT_URL}/client/v1/client${req.url}`, {
+      method: req.method,
+      headers: {
+        ...req.headers,
+        host: CLIENT_LOCAL_HOST,
+        "Content-type": "application/json",
+        "Cache-control": "no-cache",
+      },
+    }).catch(console.log);
 
-  const result = await response.json();
+    const result = await response.json();
 
-  return res.status(response.status).send(result);
-});
+    return res.status(response.status).send(result);
+  })
+  .put(authenticate, async (req, res) => {
+    /**
+     * #route   PUT /api/v1/client
+     * #desc    Update current client data
+     */
+    const response = await fetch(`${CLIENT_URL}/client/v1/client${req.url}`, {
+      method: req.method,
+      headers: {
+        ...req.headers,
+        host: CLIENT_LOCAL_HOST,
+        "Content-type": "application/json",
+      },
+      ...(req.body && { body: JSON.stringify(req.body) }),
+    }).catch(console.log);
+
+    const result = await response.json();
+
+    return res.status(response.status).send(result);
+  });
 
 export { router };
