4f3d2e9c09f050ac7c4c920e449f8d6a720d816d
Vasilen Tsvetkov <vasilen@7digit.io>
2022-11-11 18:54:40 +0000

    Create: admin router

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,3 +2,4 @@ export { router as VideoRouter } from "./VideoRouter.js";
 export { router as EmailRouter } from "./EmailRouter.js";
 export { router as UserRouter } from "./UserRouter.js";
 export { router as ClientRouter } from "./ClientRouter.js";
+export { router as AdminRouter } from "./AdminRouter.js";
