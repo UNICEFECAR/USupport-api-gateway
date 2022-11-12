f0fe6985089019b16d07bf674e21a4500deb4b7f
Vasilen Tsvetkov <vasilen@7digit.io>
2022-11-12 23:42:44 +0000

    Create: provider router

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -3,3 +3,4 @@ export { router as EmailRouter } from "./EmailRouter.js";
 export { router as UserRouter } from "./UserRouter.js";
 export { router as ClientRouter } from "./ClientRouter.js";
 export { router as AdminRouter } from "./AdminRouter.js";
+export { router as ProviderRouter } from "./ProviderRouter.js";
--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use("/api/v1/user", v1.UserRouter);
 app.use("/api/v1/client", v1.ClientRouter);
 app.use("/api/v1/email", v1.EmailRouter);
 app.use("/api/v1/admin", v1.AdminRouter);
+app.use("/api/v1/provider", v1.ProviderRouter);
 app.use("/api/v1/video", v1.VideoRouter);
 
 /*------------- Error middleware -------------*/
