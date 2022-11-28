cf440a8e46e2247dd5269192d190365c9b4b3396
Vasilen Tsvetkov <vasilen@7digit.io>
2022-11-28 03:11:20 +0000

    Refactor: remove not implemented route

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,4 +1,3 @@
-export { router as VideoRouter } from "./VideoRouter.js";
 export { router as EmailRouter } from "./EmailRouter.js";
 export { router as UserRouter } from "./UserRouter.js";
 export { router as ClientRouter } from "./ClientRouter.js";
