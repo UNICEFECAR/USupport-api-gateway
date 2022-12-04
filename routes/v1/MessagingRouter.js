2499e57b893722ed8b50733b48175447b3ee1d42
Vasilen Tsvetkov <vasilen@7digit.io>
2022-12-04 02:19:47 +0000

    Refactor: add routes for getting/pitting consultation socket for client/provider

--- routes/v1/MessagingRouter.js
+++ routes/v1/MessagingRouter.js
@@ -29,7 +29,6 @@ router.route("/").get(authenticate, async (req, res) => {
     headers: {
       ...req.headers,
       host: MESSAGING_LOCAL_HOST,
-      "x-user-id": req.user.user_id,
       "Content-type": "application/json",
       "Cache-control": "no-cache",
     },
@@ -58,7 +57,6 @@ router.route("/").put(authenticate, async (req, res) => {
     headers: {
       ...req.headers,
       host: MESSAGING_LOCAL_HOST,
-      "x-user-id": req.user.user_id,
       "Content-type": "application/json",
     },
     ...(req.body && { body: JSON.stringify(req.body) }),
@@ -67,5 +65,121 @@ router.route("/").put(authenticate, async (req, res) => {
   const result = await response.json();
   return res.status(response.status).send(result);
 });
+
+router
+  .route("/client-socket")
+  .get(authenticate, async (req, res) => {
+    /**
+     * #swagger.tags = ['Messaging']
+     * #swagger.method = 'GET'
+     * #swagger.path = '/messaging/client-socket'
+     * #swagger.description = 'Get a client socket by given chat ID'
+     * #swagger.security = [{ "ClientBearer": [] }]
+     * #swagger.parameters['x-language-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the language' }
+     * #swagger.parameters['x-country-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the country' }
+     * #swagger.parameters['chatId'] = { in: 'query', required: true, type: 'string', description: 'ID Of The Chat' }
+     * #swagger.responses[200] = { description: 'Socket Data' }
+     * #swagger.responses[401] = { description: 'Client Not Authorised' }
+     * #swagger.responses[404] = { description: 'Chat Not Found' }
+     */
+
+    const response = await fetch(`${MESSAGING_URL}/messaging/v1${req.url}`, {
+      method: req.method,
+      headers: {
+        ...req.headers,
+        host: MESSAGING_LOCAL_HOST,
+        "Content-type": "application/json",
+        "Cache-control": "no-cache",
+      },
+    }).catch(console.log);
+
+    const result = await response.json();
+    return res.status(response.status).send(result);
+  })
+  .put(authenticate, async (req, res) => {
+    /**
+     * #swagger.tags = ['Messaging']
+     * #swagger.method = 'PUT'
+     * #swagger.path = '/messaging/client-socket'
+     * #swagger.description = 'Update the client socket ID for a given chat ID'
+     * #swagger.security = [{ "ClientBearer": [] }]
+     * #swagger.parameters['x-language-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the language' }
+     * #swagger.parameters['x-country-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the country' }
+     * #swagger.parameters['obj'] = { in: 'body', schema: { $chatId: '22e3b2f6-5c95-4044-b444-592b5d41338a', $socketId: 'ySjFsdF9JwVOJvbDAAAJ' } }
+     * #swagger.responses[200] = { description: 'Updated Chat Data Object' }
+     * #swagger.responses[401] = { description: 'Client Not Authorised' }
+     * #swagger.responses[404] = { description: 'Chat Not Found' }
+     */
+    const response = await fetch(`${MESSAGING_URL}/messaging/v1${req.url}`, {
+      method: req.method,
+      headers: {
+        ...req.headers,
+        host: MESSAGING_LOCAL_HOST,
+        "Content-type": "application/json",
+      },
+      ...(req.body && { body: JSON.stringify(req.body) }),
+    }).catch(console.log);
+
+    const result = await response.json();
+    return res.status(response.status).send(result);
+  });
+
+router
+  .route("/provider-socket")
+  .get(authenticate, async (req, res) => {
+    /**
+     * #swagger.tags = ['Messaging']
+     * #swagger.method = 'GET'
+     * #swagger.path = '/messaging/provider-socket'
+     * #swagger.description = 'Get a provider socket by given chat ID'
+     * #swagger.security = [{ "ProviderBearer": [] }]
+     * #swagger.parameters['x-language-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the language' }
+     * #swagger.parameters['x-country-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the country' }
+     * #swagger.parameters['chatId'] = { in: 'query', required: true, type: 'string', description: 'ID Of The Chat' }
+     * #swagger.responses[200] = { description: 'Socket Data' }
+     * #swagger.responses[401] = { description: 'Provider Not Authorised' }
+     * #swagger.responses[404] = { description: 'Chat Not Found' }
+     */
+
+    const response = await fetch(`${MESSAGING_URL}/messaging/v1${req.url}`, {
+      method: req.method,
+      headers: {
+        ...req.headers,
+        host: MESSAGING_LOCAL_HOST,
+        "Content-type": "application/json",
+        "Cache-control": "no-cache",
+      },
+    }).catch(console.log);
+
+    const result = await response.json();
+    return res.status(response.status).send(result);
+  })
+  .put(authenticate, async (req, res) => {
+    /**
+     * #swagger.tags = ['Messaging']
+     * #swagger.method = 'PUT'
+     * #swagger.path = '/messaging/provider-socket'
+     * #swagger.description = 'Update the provider socket ID for a given chat ID'
+     * #swagger.security = [{ "ProviderBearer": [] }]
+     * #swagger.parameters['x-language-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the language' }
+     * #swagger.parameters['x-country-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the country' }
+     * #swagger.parameters['obj'] = { in: 'body', schema: { $chatId: '22e3b2f6-5c95-4044-b444-592b5d41338a', $socketId: 'ySjFsdF9JwVOJvbDAAAJ' } }
+     * #swagger.responses[200] = { description: 'Updated Chat Data Object' }
+     * #swagger.responses[401] = { description: 'Provider Not Authorised' }
+     * #swagger.responses[404] = { description: 'Chat Not Found' }
+     */
+    const response = await fetch(`${MESSAGING_URL}/messaging/v1${req.url}`, {
+      method: req.method,
+      headers: {
+        ...req.headers,
+        host: MESSAGING_LOCAL_HOST,
+        "Content-type": "application/json",
+      },
+      ...(req.body && { body: JSON.stringify(req.body) }),
+    }).catch(console.log);
+
+    const result = await response.json();
+    return res.status(response.status).send(result);
+  });
 
 export { router };
