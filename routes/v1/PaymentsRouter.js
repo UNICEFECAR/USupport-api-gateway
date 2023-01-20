acc75b8c0a2274bfa7d87d52859da7bf11385d8c
Sebastian Galustyan <sebastian@7digit.io>
2023-01-20 13:30:16 +0000

    Add: new routes to PaymentRouter '/one-time/create-payment-intent' and '/one-time/webhook'

--- routes/v1/PaymentsRouter.js
+++ routes/v1/PaymentsRouter.js
@@ -1,16 +1,42 @@
 import express from "express";
 import fetch from "node-fetch";
 
+import { authenticate } from "#middlewares/auth";
+
 const router = express.Router();
 
 const PAYMENTS_LOCAL_HOST = "http://localhost:3004";
 
 const PAYMENTS_URL = process.env.PAYMENTS_URL;
 
-router.route("/one-time/create-payment-intent").post(async (req, res) => {
+router
+  .route("/one-time/create-payment-intent")
+  .post(authenticate, async (req, res) => {
+    /**
+     * #swagger.tags = ['Payments']
+     * #swagger.description = 'Endpoint to test the Payments service.'
+     */
+
+    const response = await fetch(`${PAYMENTS_URL}/payments/v1${req.url}`, {
+      method: req.method,
+      headers: {
+        ...req.headers,
+        host: PAYMENTS_LOCAL_HOST,
+        "x-user-id": req.user.user_id,
+        "Content-type": "application/json",
+      },
+      ...(req.body && { body: JSON.stringify(req.body) }),
+    }).catch(console.log);
+
+    const result = await response?.json();
+
+    return res.status(response?.status).send(result);
+  });
+
+router.route("/one-time/webhook").post(async (req, res) => {
   /**
    * #swagger.tags = ['Payments']
-   * #swagger.description = 'Endpoint to test the Payments service.'
+   * #swagger.description = 'Endpoint to handle Webhooks received from Stripe.'
    */
   const response = await fetch(`${PAYMENTS_URL}/payments/v1${req.url}`, {
     method: req.method,
@@ -26,4 +52,5 @@ router.route("/one-time/create-payment-intent").post(async (req, res) => {
 
   return res.status(response?.status).send(result);
 });
+
 export { router };
