19ce8aba9a00a7f2c48b3c191c8128e1d7e1bfbf
Sebastian Galustyan <sebastian@7digit.io>
2023-01-27 00:59:19 +0000

    Create: new route '/one-time/refund'

--- routes/v1/PaymentsRouter.js
+++ routes/v1/PaymentsRouter.js
@@ -14,7 +14,16 @@ router
   .post(authenticate, async (req, res) => {
     /**
      * #swagger.tags = ['Payments']
-     * #swagger.description = 'Endpoint to test the Payments service.'
+     * #swagger.method = 'POST'
+     * #swagger.path = '/payments/one-time/create-payment-intent'
+     * #swagger.description = 'Post create payment intent'
+     * #swagger.security = [{ "AnyUserBearer": [] }]
+     * #swagger.parameters['x-language-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the language' }
+     * #swagger.parameters['x-country-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the country' }
+     * #swagger.parameters['obj'] = { in: 'body', schema: { consultationId: '90838065-6c13-4b43-ad8e-9f10c5b6ae7e' } }
+     * #swagger.responses[200] = { description: 'Consultation ID Object' }
+     * #swagger.responses[401] = { description: 'User Not Authorised' }
+     * #swagger.responses[404] = { description: 'Consultation Not Found' }
      */
 
     const response = await fetch(`${PAYMENTS_URL}/payments/v1${req.url}`, {
@@ -67,6 +76,37 @@ router.route("/one-time/history").get(authenticate, async (req, res) => {
       "x-user-id": req.user.user_id,
       "Content-type": "application/json",
     },
+  }).catch(console.log);
+
+  const result = await response?.json();
+
+  return res.status(response?.status).send(result);
+});
+
+router.route("/one-time/refund").post(authenticate, async (req, res) => {
+  /**
+   * #swagger.tags = ['Payments']
+   * #swagger.method = 'POST'
+   * #swagger.path = '/payments/one-time/refund'
+   * #swagger.description = 'Post refund existent payment.'
+   * #swagger.security = [{ "AnyUserBearer": [] }]
+   * #swagger.parameters['x-language-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the language' }
+   * #swagger.parameters['x-country-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the country' }
+   * #swagger.parameters['obj'] = { in: 'body', schema: { consultationId: '90838065-6c13-4b43-ad8e-9f10c5b6ae7e' } }
+   * #swagger.responses[200] = { description: 'Consultation ID Object' }
+   * #swagger.responses[401] = { description: 'User Not Authorised' }
+   * #swagger.responses[404] = { description: 'Consultation Not Found' }
+   */
+
+  const response = await fetch(`${PAYMENTS_URL}/payments/v1${req.url}`, {
+    method: req.method,
+    headers: {
+      ...req.headers,
+      host: PAYMENTS_LOCAL_HOST,
+      "x-user-id": req.user.user_id,
+      "Content-type": "application/json",
+    },
+    ...(req.body && { body: JSON.stringify(req.body) }),
   }).catch(console.log);
 
   const result = await response?.json();
