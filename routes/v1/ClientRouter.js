953dfa8649777e4b6427976659cc30b89b85461e
Georgi 7DIGIT <georgi@7digit.io>
2023-03-15 16:20:55 +0200

    Add: unblock slot route in the ClientRouter

--- routes/v1/ClientRouter.js
+++ routes/v1/ClientRouter.js
@@ -542,4 +542,36 @@ router.route("/check-coupon").get(authenticate, async (req, res) => {
   return res.status(response.status).send(result);
 });
 
+router
+  .route("/consultation/unblock-slot")
+  .put(authenticate, async (req, res) => {
+    /**
+     * #swagger.tags = ['Client']
+     * #swagger.method = 'PUT'
+     * #swagger.path = '/client/consultation/unblock-slot'
+     * #swagger.description = 'Client to unblock a slot'
+     * #swagger.security = [{ "AnyUserBearer": [] }]
+     * #swagger.parameters['x-language-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the language' }
+     * #swagger.parameters['x-country-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the country' }
+     * #swagger.parameters['obj'] = { in: 'body', schema: { $consultationId: '22e3b2f6-5c95-4044-b444-592b5d41338a'} }
+     * #swagger.responses[200] = { description: 'Success Status' }
+     * #swagger.responses[401] = { description: 'User Not Authorised' }
+     * #swagger.responses[404] = { description: 'Consultation Not Found' }
+     */
+    const response = await fetch(`${CLIENT_URL}/client/v1${req.url}`, {
+      method: req.method,
+      headers: {
+        ...req.headers,
+        host: CLIENT_LOCAL_HOST,
+        "x-user-id": req.user.user_id,
+        "Content-type": "application/json",
+      },
+      ...(req.body && { body: JSON.stringify(req.body) }),
+    }).catch(console.log);
+
+    const result = await response.json();
+
+    return res.status(response.status).send(result);
+  });
+
 export { router };
