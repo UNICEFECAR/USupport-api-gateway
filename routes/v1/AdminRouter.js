506b5f49d9f8045c9a682eb15370d780a089713c
7Digit-Main <it-support@7digit.io>
2025-07-10 16:35:36 +0300

    Add: platform suggestion endpoints

--- routes/v1/AdminRouter.js
+++ routes/v1/AdminRouter.js
@@ -885,6 +885,38 @@ router
   });
 
 router
+  .route("/statistics/platform-suggestions")
+  .get(authenticateAdmin, authorizeAdmin("country"), async (req, res) => {
+    /**
+     * #swagger.tags = ['Admin']
+     * #swagger.method = 'GET'
+     * #swagger.path = '/admin/statistics/platform-suggestions'
+     * #swagger.description = 'Get all platform suggestions'
+     * #swagger.security = [{ "CountryAdminBearer": [] }]
+     * #swagger.parameters['x-language-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the language' }
+     * #swagger.parameters['x-country-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the country' }
+     * #swagger.parameters['countryId'] = { in: 'query', required: true, type: 'string', description: 'ID of the country of the admin' }
+     * #swagger.responses[200] = { description: 'Information Portal Suggestions Data Object' }
+     * #swagger.responses[401] = { description: 'Admin Not Authorised' }
+     * #swagger.responses[401] = { description: 'No Permissions' }
+     */
+
+    const response = await fetch(`${ADMIN_URL}/admin/v1${req.url}`, {
+      method: req.method,
+      headers: {
+        ...req.headers,
+        host: ADMIN_LOCAL_HOST,
+        "Content-type": "application/json",
+        "Cache-control": "no-cache",
+      },
+    }).catch(console.log);
+
+    const result = await response.json();
+
+    return res.status(response.status).send(result);
+  });
+
+router
   .route("/statistics/client-ratings")
   .get(authenticateAdmin, authorizeAdmin("country"), async (req, res) => {
     /**
