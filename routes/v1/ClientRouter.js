e03455ed020df13d729e2ef83ca0177ae72115ff
7Digit-main <it-support@7digit.io>
2025-09-30 16:20:25 +0300

    Add: generate mood tracker report endpoint with Swagger documentation

--- routes/v1/ClientRouter.js
+++ routes/v1/ClientRouter.js
@@ -334,6 +334,36 @@ router
     return res.status(response.status).send(result);
   });
 
+router.route("/mood-tracker/report").get(authenticate, async (req, res) => {
+  /**
+   * #swagger.tags = ['Client']
+   * #swagger.method = 'GET'
+   * #swagger.path = '/client/mood-tracker/report'
+   * #swagger.description = 'Generate mood tracker report'
+   * #swagger.security = [{ "ClientBearer": [] }]
+   * #swagger.parameters['x-language-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the language' }
+   * #swagger.parameters['x-country-alpha-2'] = { in: 'header', required: true, type: 'string', description: 'Alpha 2 code of the country' }
+   * #swagger.parameters['startDate'] = { in: 'query', required: true, type: 'string', description: 'The start date' }
+   * #swagger.parameters['endDate'] = { in: 'query', required: true, type: 'string', description: 'The end date' }
+   * #swagger.responses[200] = { description: 'Mood Tracker Report Data Object' }
+   * #swagger.responses[401] = { description: 'Client Not Authorised' }
+   */
+
+  const response = await fetch(`${CLIENT_URL}/client/v1${req.url}`, {
+    method: req.method,
+    headers: {
+      ...req.headers,
+      host: CLIENT_LOCAL_HOST,
+      "x-user-id": req.user.user_id,
+      "Content-type": "application/json",
+    },
+  }).catch(console.log);
+
+  const result = await response.json();
+
+  return res.status(response.status).send(result);
+});
+
 router.route("/mood-tracker/today").get(authenticate, async (req, res) => {
   /**
    * #swagger.tags = ['Client']
