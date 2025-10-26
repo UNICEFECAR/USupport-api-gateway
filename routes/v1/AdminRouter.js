329c13c729188c1e97226465b1984d209fbd7020
7Digit-Main <it-support@7digit.io>
2025-10-26 15:43:49 +0200

    Cerate: statistics endpoint for P&H

--- routes/v1/AdminRouter.js
+++ routes/v1/AdminRouter.js
@@ -2319,4 +2319,35 @@ router.get(
   }
 );
 
+router.get(
+  "/statistics/playandheal-visits",
+  authenticateAdmin,
+  async (req, res) => {
+    /**
+     * #swagger.tags = ['Admin']
+     * #swagger.method = 'GET'
+     * #swagger.path = '/admin/statistics/playandheal-visits'
+     * #swagger.description = 'Get all play and heal visits'
+     * #swagger.security = [{ "GlobalAdminBearer": [] }]
+     * #swagger.responses[200] = { description: 'All play and heal visits' }
+     * #swagger.responses[401] = { description: 'Admin Not Authorised' }
+     * #swagger.responses[401] = { description: 'No Permissions' }
+     */
+
+    const response = await fetch(`${ADMIN_URL}/admin/v1${req.url}`, {
+      method: req.method,
+      headers: {
+        ...req.headers,
+        host: ADMIN_LOCAL_HOST,
+        "Content-type": "application/json",
+        "Cache-control": "no-cache",
+      },
+    }).catch(console.log);
+
+    const result = await response.json();
+
+    return res.status(response.status).send(result);
+  }
+);
+
 export { router };
